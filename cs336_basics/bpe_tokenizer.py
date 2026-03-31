a7f197ec37bb6f54d8a71dd47cb3c1915e873b44
adamwild <adam.wild@ensta-bretagne.org>
2026-03-31 17:37:24 +0200

    move current merge strat to naive_merge

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 from multiprocessing import Pool
 from itertools import repeat
 from cs336_basics.pretokenization_example import find_chunk_boundaries
+from cs336_basics.naive_merge import naive_merge_loop
 
 def pre_tokenize(args):
     (start, end), input_path, special_tokens = args
@@ -64,117 +65,6 @@ def split_frequency_table(frequency_table):
 
     return s_frequency_table
 
-def apply_merge(s_frequency_table, merge_pair):
-    """_summary_
-
-    Args:
-        s_frequency_table (dict[tuple[bytes], int]): {(b'l', b'o', b'w'): 1, (b' ', b'l', b'o', b'w'): 4,
-        merge_pair (tuple[bytes]): (b's', b't')
-    """
-    new_s_frequency_table = {}
-    new_byte_pair_count = {}
-    
-    b1_target, b2_target = merge_pair
-    merged_token = b1_target + b2_target
-
-    for s_token, count in s_frequency_table.items():
-        # Handle single-byte tokens immediately
-        if len(s_token) == 1:
-            # new_byte_pair_count[s_token] = new_byte_pair_count.get(s_token, 0) + count
-            continue
-
-        if len(s_token) == 2:
-            b1, b2 = s_token
-
-            if (b1, b2) == merge_pair:
-                # new_byte_pair_count[merged_token] = new_byte_pair_count.get(merged_token, 0) + count
-                # new_s_frequency_table[merged_token] = new_s_frequency_table.get(merged_token, 0) + count
-                pass
-            else:
-                new_byte_pair_count[(b1, b2)] = new_byte_pair_count.get((b1, b2), 0) + count
-                new_s_frequency_table[(b1, b2)] = new_s_frequency_table.get((b1, b2), 0) + count
-            continue
-
-        # 3 or more bytes
-        new_s_token = []
-
-        ind_byte = 0
-
-        # If b1 belongs to a merged pair from his predecessor, consider b1 as the merged bytes
-        rename_b1 = False
-
-        while ind_byte < len(s_token)-1:
-            b1, b2 = s_token[ind_byte], s_token[ind_byte+1]
-
-            if rename_b1:
-                b1 = merged_token
-                rename_b1 = False
-
-            # Last two bytes
-            if ind_byte == len(s_token)-2:
-                new_byte_pair_count[(b1, b2)] = new_byte_pair_count.get((b1, b2), 0) + count
-                new_s_token.append(b1)
-                new_s_token.append(b2)
-                break
-
-            b3 = s_token[ind_byte+2]
-
-            # We write the possible cases, b_i means a byte that doesn't merge, c_i is a byte that belongs to a mergure
-            if (b1, b2) != merge_pair:
-                new_s_token.append(b1)
-
-                # b1, b2, b3
-                if (b2, b3) != merge_pair:
-                    new_byte_pair_count[(b1, b2)] = new_byte_pair_count.get((b1, b2), 0) + count
-                    ind_byte += 1
-
-                # b1, c2, c3
-                else:
-                    new_byte_pair_count[(b1, merged_token)] = new_byte_pair_count.get((b1, merged_token), 0) + count
-                    ind_byte += 2
-
-                    # We restart the loop on c3 which belongs to a merge
-                    rename_b1 = True
-
-                    # c2, c3 is the end of the token
-                    if ind_byte == len(s_token)-1:
-                        new_s_token.append(merged_token)
-
-            else:
-                new_s_token.append(merged_token)
-
-                if ind_byte + 3 <= len(s_token)-1:
-                    b4 = s_token[ind_byte+3]
-
-                    # c1, c2, b3, b4
-                    if (b3, b4) != merge_pair:
-                        new_byte_pair_count[(merged_token, b3)] = new_byte_pair_count.get((merged_token, b3), 0) + count
-                        ind_byte += 2
-
-                    # c1, c2, c3, c4
-                    else:
-                        new_byte_pair_count[(merged_token, merged_token)] = new_byte_pair_count.get((merged_token, merged_token), 0) + count
-                        ind_byte += 3
-                        rename_b1 = True
-
-                # c1, c2, b3]
-                else:
-                    new_byte_pair_count[(merged_token, b3)] = new_byte_pair_count.get((merged_token, b3), 0) + count
-                    ind_byte += 2
-                    new_s_token.append(b3)
-
-        new_s_frequency_table[tuple(new_s_token)] = new_s_frequency_table.get(tuple(new_s_token), 0) + count
-
-    return new_s_frequency_table, new_byte_pair_count
-
-def get_next_merge(byte_pair_count):
-    # Next pair to merge by finding pairs with highest counts, ties are broken lexicographically
-    max_count = max(count for count in byte_pair_count.values())
-    tied_first_place = [byte_pair for byte_pair, count in byte_pair_count.items() if count == max_count]
-    next_pair_merge = max(tied_first_place)
-    
-    return next_pair_merge
-
 def init_vocab(special_tokens):
     vocab = {num_token: token.encode("utf-8") for num_token, token in enumerate(special_tokens)}
 
@@ -191,7 +81,9 @@ def train_bpe(
     special_tokens: list[str],
     **kwargs,
 ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-    
+    import time
+    ref_time = time.time()
+
     if type(input_path) == str:
         input_path = pathlib.Path(input_path)
     
@@ -224,12 +116,9 @@ def train_bpe(
     merges = []
     vocab = init_vocab(special_tokens)
     while len(vocab) < vocab_size:
-        new_merge = get_next_merge(byte_pair_count)
-        merges.append(new_merge)
-
-        s_frequency_table, byte_pair_count = apply_merge(s_frequency_table, new_merge)
-
-        vocab[len(vocab)] = new_merge[0] + new_merge[1]
+        vocab, merges, s_frequency_table, byte_pair_count = naive_merge_loop(vocab, merges, s_frequency_table, byte_pair_count)
+        
+    print(time.time() - ref_time)
 
     return vocab, merges
 
