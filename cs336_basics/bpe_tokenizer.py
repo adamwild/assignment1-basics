360e74d622609b4aaf235582e09e58259be515b7
adamwild <adam.wild@ensta-bretagne.org>
2026-04-02 22:48:14 +0200

    First BPE train exercise

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -1,13 +1,13 @@
 import os
+import pickle
 import regex as re
 import multiprocessing
-from collections import defaultdict
 from multiprocessing import Pool
 from itertools import repeat
 from cs336_basics.pretokenization_example import find_chunk_boundaries
 from cs336_basics.naive_merge import get_next_merge, reduce_s_token
 
-def pre_tokenize(args):
+def compute_frequency_tables(args):
     (start, end), input_path, special_tokens = args
 
     with open(input_path, "rb") as f:
@@ -68,15 +68,7 @@ def init_vocab(special_tokens):
 
     return vocab
 
-def train_bpe(
-    input_path: str | os.PathLike,
-    vocab_size: int,
-    special_tokens: list[str],
-    **kwargs,
-) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-    import time
-    ref_time = time.time()
-
+def pre_tokenize(input_path, special_tokens):
     if type(input_path) == str:
         input_path = pathlib.Path(input_path)
     
@@ -90,11 +82,9 @@ def train_bpe(
     args_list = list(zip(start_end_pairs, repeat(input_path), repeat(special_tokens)))
 
     with Pool() as pool:
-        frequency_tables = pool.map(pre_tokenize, args_list)
+        frequency_tables = pool.map(compute_frequency_tables, args_list)
 
     # Combining all frequency_tables
-    frequency_table = defaultdict(int)
-
     # id_token_count = {168: [[b' ', b'l', b'ow'], 7]}
     id_token_count = {}
     pre_tokens = []
@@ -124,7 +114,10 @@ def train_bpe(
                 for b1, b2 in zip(s_pre_token[:-1], s_pre_token[1:]):
                     byte_pair_index.setdefault((b1, b2), []).append(pre_tokens.index(pre_token))
                     byte_pair_count[(b1, b2)] = byte_pair_count.get((b1, b2), 0) + pre_token_count
-            
+
+    return id_token_count, byte_pair_count, byte_pair_index
+
+def merge_loop(vocab_size, id_token_count, byte_pair_count, byte_pair_index, special_tokens):     
     # Merge loop
     merges = []
     vocab = init_vocab(special_tokens)
@@ -144,15 +137,73 @@ def train_bpe(
 
     return vocab, merges
 
+def save_checkpoint(dict_datas, folder_checkpoints):
+    """Save several dictionaries as json files
+
+    Args:
+        dict_datas (dict[str]=dict): dict['save_name']=dict_to_save
+    """
+    folder_checkpoints.mkdir(parents=True, exist_ok=True)
+
+    for name_save, dict_to_save in dict_datas.items():
+        with open(folder_checkpoints / name_save, "wb") as f:
+            pickle.dump(dict_to_save, f)
+
+def read_checkpoint(datapoint_name, folder_checkpoints):
+
+    with open(folder_checkpoints / datapoint_name, "rb") as f:
+        datapoint = pickle.load(f)
+
+    if type(datapoint) == dict:
+        for key, val in datapoint.items():
+            print(key, val)
+
+    else: 
+        print(datapoint)
+
+def train_bpe(
+    input_path: str | os.PathLike,
+    vocab_size: int,
+    special_tokens: list[str],
+    **kwargs,
+) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    
+    id_token_count, byte_pair_count, byte_pair_index = pre_tokenize(input_path, special_tokens)
+
+    if 'folder_path' in kwargs:
+        print("gello?")
+        dicts_to_save = {'id_token_count': id_token_count, 'byte_pair_count': byte_pair_count, 'byte_pair_index': byte_pair_index}
+        save_checkpoint(dicts_to_save, kwargs['folder_path'])
+
+    vocab, merges = merge_loop(vocab_size, id_token_count, byte_pair_count, byte_pair_index, special_tokens)
+
+    if 'folder_path' in kwargs:
+        dicts_to_save = {'vocab': vocab, 'merges': merges}
+        save_checkpoint(dicts_to_save, kwargs['folder_path'])
+
+    return vocab, merges
+
+
+
 if __name__ == "__main__":
     import pathlib
     data_path = (pathlib.Path(__file__).resolve().parent.parent) / "data"
     fixtures_path = (pathlib.Path(__file__).resolve().parent.parent) / 'tests' / "fixtures"
+    checkpoints_path = (pathlib.Path(__file__).resolve().parent.parent) / 'cs336_basics' / 'checkpoints'
     test_file = data_path / 'basic_bpe_example.txt'
     special_tokens=["<|endoftext|>"]
 
     corpus_en_file = fixtures_path / 'corpus.en'
 
-    vocab, merges = train_bpe(test_file, 263, special_tokens)
-    print(merges)
+    tiny_stories_file = data_path / "TinyStoriesV2-GPT4-train.txt"
+    OpenWebText_file = data_path / "owt_train.txt"
+
+    # First exercise
+    # vocab, merges = train_bpe(tiny_stories_file, 10000, special_tokens, folder_path=checkpoints_path)
+
+    # Second running exercise 22:45
+    vocab, merges = train_bpe(OpenWebText_file, 32000, special_tokens, folder_path=checkpoints_path)
+
+    # read_checkpoint("vocab", checkpoints_path / 'TinyStories_10000')
+
 
